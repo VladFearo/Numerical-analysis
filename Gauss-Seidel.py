0923c30749712ced6c628dea6d48288098d8b043
VladFearo <69718459+VladFearo@users.noreply.github.com>
2022-04-10 18:53:37 +0300

    added gauss seidel algorithm using numpy

---
+++ Gauss-Seidel.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+a = np.array(
+    [[50, 0, -15],
+    [-5, 50, -25],
+    [1, -5, 50]])
+b = np.array([50, -25, 20])
+
+x = np.linalg.solve(a, b)
+print(x)
+
+
+def gauss_seidel(A, b):
+
+    xn, yn, zn = 0, 0, 0
+    counter = 1
+    if dominant_pivot(A):
+        while True:
+            print(f'Iteration number {counter}:')
+            print(f'{xn=} {yn=} {zn=}')
+            xn = (b[0] - A[0][1] * yn - A[0][2] * zn) / A[0][0]
+            yn = (b[1] - A[1][0] * xn - A[1][2] * zn) / A[1][1]
+            zn = (b[2] - A[2][0] * xn - A[2][1] * yn) / A[2][2]
+            counter += 1
+            if xn == x[0] and yn == x[1] and zn == x[2]:
+                print('__________________________')
+                print(f'The solution vector is: ')
+                return list(map(lambda y: round(y, 8), [xn, yn, zn]))
+
+
+def dominant_pivot(matrix):
+    j = 0
+    for i in matrix:
+        abs_list = list(map(abs, i))
+        if i[j] < sum(abs_list) - abs(i[j]):
+            print("Not a dominant pivot")
+            return False
+        j += 1
+    return True
+
+
+print(gauss_seidel(a, b))
