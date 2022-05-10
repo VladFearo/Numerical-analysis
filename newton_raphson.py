8809c473278095b51186846d49ab7dd9e210424c
idobe2 <idobenoun@gmail.com>
2022-05-10 18:35:16 +0300

    files updated

--- newton_raphson.py
+++ newton_raphson.py
@@ -86,7 +86,7 @@ def deriv_poly(poly):
 def newton_raphson(a, b, f, f_d, epsilon):
     m = (a + b) / 2
     i = 1
-    if f(a) > 0 > f(b) or f(a) < 0 < f(b):
+    if f(a) > 0 > f(b) or f(a) < 0 < f(b): # hash
         while True:
             temp = m
             m = m - f(m)/f_d(m)
