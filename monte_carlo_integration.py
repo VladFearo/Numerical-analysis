695f66878afafa8acc51dde505b307654ad62d72
VladFearo <69718459+VladFearo@users.noreply.github.com>
2022-07-10 12:52:47 +0300

    modifications to save space

--- monte_carlo_integration.py
+++ monte_carlo_integration.py
@@ -3,33 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 0
-b = np.pi
-N = 1000
+b = 1
+N = 2500
 xrand = np.zeros(N)
 for i in range(len(xrand)):
-    xrand[i] = random.uniform(a,b)
+    xrand[i] = random.uniform(a, b)
 
 def func(x):
-    return np.sin(x)
-
-integral = 0.0
-answer = (b-a)/float(N)*integral
-
-
+    return x**2
 
 areas = []
 
-for i in range(N):
-    xrand = np.zeros(N)
-    for i in range(len(xrand)):
-        xrand[i] = random.uniform(a, b)
-        integral = 0.0
-    for i in range(N):
-        integral += func(xrand[i])
-    answer = (b-a)/float(N)*integral
-    areas.append(answer)
+for _ in range(N):
+    integral = 0.0
+    for _ in range(N):
+        integral += func(random.uniform(a, b))
+    areas.append((b-a)/float(N)*integral)
 plt.title("Distribution of Areas calculated")
-plt.hist(areas, bins=30, ec='black')
+plt.hist(areas, bins=31, ec='black')
 plt.xlabel("Areas")
 plt.show()
 
